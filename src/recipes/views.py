f9d23e5a088da4119d71ecb706affaf8e4fe0da5
Jacob DiBiase <jakesback93@hotmail.com>
2024-12-28 21:28:20 -0700

    Add and register recipe list template/url

--- src/recipes/views.py
+++ src/recipes/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Recipe
 
 # Create your views here.
 def home(request):
     return render(request, 'recipes/recipes_home.html')
+
+class RecipeListView(ListView):
+    model = Recipe
+    template_name = 'recipes/recipe_details.html'
