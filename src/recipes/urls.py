b47ad469115c3931b46adaf5edfdff7ce5af023c
Jacob DiBiase <jakesback93@hotmail.com>
2024-12-28 21:28:20 -0700

    Add and register recipe list template/url

--- src/recipes/urls.py
+++ src/recipes/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home
+from .views import home, RecipeListView
 
 app_name = 'recipes'
 
 urlpatterns = [
     path('', home),
+    path('list/', RecipeListView.as_view(), name='list')
 ]
