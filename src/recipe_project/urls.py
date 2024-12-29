7f66035014506fbd1471bf904315f7239d193378
Jacob DiBiase <jakesback93@hotmail.com>
2024-12-28 23:22:05 -0700

    Adjust the recipes list styling

--- src/recipe_project/urls.py
+++ src/recipe_project/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('recipes.urls')),
+    path('recipes/', include('recipes.urls'))
 ]
 
 # Extend url patterns parameter to include media information
