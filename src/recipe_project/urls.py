81d64b113511d854f605e888013ed911f04df1f2
Jacob DiBiase <jakesback93@hotmail.com>
2025-01-03 00:20:48 -0700

    Register login URL to the project

--- src/recipe_project/urls.py
+++ src/recipe_project/urls.py
@@ -18,10 +18,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import login_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('recipes.urls', namespace='recipes')),
+    path('login/', login_view, name='login')
 ]
 
 # Extend url patterns parameter to include media information
