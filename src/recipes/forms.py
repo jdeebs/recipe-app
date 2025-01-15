0aa2f81e0c269053c68f401c536cd3b04b0c7d75
Jacob DiBiase <jakesback93@hotmail.com>
2025-01-14 23:05:02 -0700

    Init user form

---
+++ src/recipes/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+
+CHART__CHOICES = {
+    # Specify choices as a tuple
+    # When user selects "Bar chart" it is stored as #1
+    {'#1', 'Bar chart'},
+    {'#2', 'Pie chart'},
+    {'#3', 'Line chart'}
+}
+
+class RecipesSearchForm(forms.Form):
+    recipe_title = forms.CharField(max_length=120)
+    chart_type = forms.ChoiceField(choices=CHART__CHOICES)
