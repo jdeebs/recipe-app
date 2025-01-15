4374e97e63da54eeedd1cf5ab3f13302078574b9
Jacob DiBiase <jakesback93@hotmail.com>
2025-01-15 00:25:54 -0700

    Fix syntax of chart choices

--- src/recipes/forms.py
+++ src/recipes/forms.py
@@ -1,12 +1,12 @@
 from django import forms
 
-CHART__CHOICES = {
+CHART__CHOICES = (
     # Specify choices as a tuple
     # When user selects "Bar chart" it is stored as #1
-    {'#1', 'Bar chart'},
-    {'#2', 'Pie chart'},
-    {'#3', 'Line chart'}
-}
+    ('#1', 'Bar chart'),
+    ('#2', 'Pie chart'),
+    ('#3', 'Line chart')
+)
 
 class RecipesSearchForm(forms.Form):
     recipe_title = forms.CharField(max_length=120)
