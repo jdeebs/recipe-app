d86840b04f9fdbcd72e041fef3b803a090944de6
Jacob DiBiase <jakesback93@hotmail.com>
2025-01-15 18:37:26 -0700

    Fix django_filters import errors

--- src/recipes/filters.py
+++ src/recipes/filters.py
@@ -3,16 +3,16 @@ from django.db.models import Q
 from .models import Recipe
 
 class RecipeFilter(django_filters.FilterSet):
-    search = django_filters.CharFilter(method='filter_all_fields', label='Search')
+    search = django_filters.CharFilter(method='filter_all_fields')
 
     class Meta:
         model = Recipe
         fields = []
 
-    def filter_all_fields(self, queryset, name, value):
+    def filter_all_fields(self, queryset, value):
         # Filters across recipe title, ingredients, and difficulty
         return queryset.filter(
-            Q(name__icontains=value) | 
-            Q(ingredients__icontains=value) | 
+            Q(name__icontains=value) |
+            Q(ingredients__icontains=value) |
             Q(difficulty__icontains=value)
         )
