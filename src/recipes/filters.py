fb3d2d9b25ee4d4fd9bd927ff1650312ba7c823a
Jacob DiBiase <jakesback93@hotmail.com>
2025-01-15 18:48:53 -0700

    Remove redundant ListView to fix search function

--- src/recipes/filters.py
+++ src/recipes/filters.py
@@ -3,13 +3,13 @@ from django.db.models import Q
 from .models import Recipe
 
 class RecipeFilter(django_filters.FilterSet):
-    search = django_filters.CharFilter(method='filter_all_fields')
+    search = django_filters.CharFilter(method='filter_search')
 
     class Meta:
         model = Recipe
         fields = []
 
-    def filter_all_fields(self, queryset, value):
+    def filter_search(self, queryset, name, value):
         # Filters across recipe title, ingredients, and difficulty
         return queryset.filter(
             Q(name__icontains=value) |
