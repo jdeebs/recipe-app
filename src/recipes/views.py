60ff06b48fe8dff1779112988bc6ad25c01379fc
Jacob DiBiase <jakesback93@hotmail.com>
2025-01-15 18:11:06 -0700

    Update RecipeListView to include FilterView for search functionality

--- src/recipes/views.py
+++ src/recipes/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+# For search functionality
+from django_filters.views import FilterView
+from .filters import RecipeFilter
 # To protect a CBV
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Recipe
@@ -8,9 +11,16 @@ from .models import Recipe
 def home(request):
     return render(request, 'recipes/recipes_home.html')
 
-class RecipeListView(LoginRequiredMixin, ListView):
+class RecipeListView(LoginRequiredMixin, ListView, FilterView):
     model = Recipe
     template_name = 'recipes/recipe_list.html'
+    context_object_name = 'recipes'
+    filterset_class = RecipeFilter
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['search_placeholder'] = 'Search recipes by title, ingredients, or difficulty'
+        return context
 
 class RecipeDetailView(LoginRequiredMixin, DetailView):
     model = Recipe
