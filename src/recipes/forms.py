5619ba7780eb113bc8407df246394c85b32213fc
Jacob DiBiase <jakesback93@hotmail.com>
2025-01-18 20:50:39 -0700

    Update chart selection style

--- src/recipes/forms.py
+++ src/recipes/forms.py
@@ -1,9 +1,9 @@
 from django import forms
 
 CHART__CHOICES = (
-    ('#1', 'Bar chart'),
-    ('#2', 'Pie chart'),
-    ('#3', 'Line chart')
+    ('#1', 'Bar chart: Ingredient Frequency'),
+    ('#2', 'Pie chart: Recipes by Difficulty'),
+    ('#3', 'Line chart: Recipes Total Time')
 )
 
 class RecipeChartForm(forms.Form):
